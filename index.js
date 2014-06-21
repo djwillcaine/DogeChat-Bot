f386aa8099fd1f221515311445a9dcb8472a8620
Will Caine <cainy393@gmail.com>
2014-06-21 12:14:21 +0100

    Revert "Fixed the omission of first word in messages"
    This reverts commit 8e16f1d1fd154435d8bb41f398dff30255825c5d.

--- index.js
+++ index.js
@@ -71,20 +71,19 @@ exports.connect = function(user, pass, callback) {
 			log("chat", "<" + data.user + "> " + msg);
 			msgArray = msg.split(" ");
 			command = msgArray.shift().toLowerCase();
-			commandMsg = msgArray.join(" ");
+			msg = msgArray.join(" ");
 			chatData = {
 				"user": data.user,
+				"message": msg,
 				"room": data.room,
 				"messageArray": msgArray,
 				"timestamp": data.timestamp
 			}
 			if (typeof onChat === 'function') {
-				chatData.message = msg;
 				onChat(chatData);
 			}
 			if (typeof commands[command] == 'function') {
 				chatData.command = command;
-				chatData.message = commandMsg;
 				commands[command](chatData);
 			} else if (contains(data.message, ["<span class='label label-success'>has tipped " + botUsername + " "]) && typeof onTip == 'function') {
 				amt = Number(data.message.split("<span class='label label-success'>has tipped ")[1].split(" ")[1]);
